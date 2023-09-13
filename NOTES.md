305cae7d29bffbe8bcc191489076fe295b9adabd
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-13 14:59:26 -0500

    initial app

---
+++ NOTES.md
@@ -0,0 +1,2 @@
+# Notes
+* IP address of `https://discord.com` is `162.159.128.233`.
---
+++ main.c
@@ -0,0 +1,48 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int socket_desc;
+char *host;
+
+const char *authToken;
+
+char gateway_url = "api/v10/gateway"
+
+int main(int argc , char *argv[]) {
+    struct sockaddr_in server;
+	char server_reply[2000];
+
+    host = argv[1];
+
+    authToken = argv[2];
+
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
+
+    server.sin_addr.s_addr = inet_addr(host);
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 80 );
+
+    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		puts("connect error");
+		return 1;
+	}
+
+    puts("Connected\n");
+
+    while (1) {
+    	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+		{
+			puts("recv failed");
+		}
+		puts("Reply received\n");
+		puts(server_reply);
+    }
+}
